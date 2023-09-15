0379c29ab8f95367ee110d1dcce6dad2eb0bce5b
ROBINSON1992 <robinsonotieno50@gmail.com>
2023-09-15 06:10:15 -0700

    variables

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<unistd.h>
+/**
+ * main - Entry block
+ * Description:  a program that prints the alphabet in lowercase
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		else
+		{
+		}
+	}
+	putchar('\n');
+	return (0);
+}
